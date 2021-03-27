de78deb5a39ae1afe7a69c186b1780f6f084c05c
Surapich Chairgulprasert <csurapich@gmail.com>
2021-03-27 09:01:02 +0700

    Now detects fish better

--- server.js
+++ server.js
@@ -10,24 +10,47 @@ try {
   process.exit(-1);
 }
 
-let messages = [];
+const wildIcon = "https://cdn.discordapp.com/emojis/666459920551510016.png?v=1";
+const waveIcon = "https://cdn.discordapp.com/emojis/749494213770477628.png?v=1";
 
 client.on("ready", () => {
   console.log("Bot is ready");
 
   client.on("message", (message) => {
     let content = message.content;
+    let embeds = message.embeds;
 
-    if (content.includes("found a wild")) {
-      var user = content.substr(0, content.indexOf("found a wild"));
+    // if (content.includes("found a wild")) {
+    //   var user = content.substr(0, content.indexOf("found a wild"));
 
-      setTimeout(() => {
-        message.channel.send(`${user}your *pokemon* is ready`);
-      }, 11 * 1000);
-    } else if (!content && message.author.id === "664508672713424926") {
-      setTimeout(() => {
-        message.channel.send(`**Fish** is ready`);
-      }, 25 * 1000);
+    //   setTimeout(() => {
+    //     message.channel.send(`${user}your *pokemon* is ready`);
+    //   }, 11 * 1000);
+    // }
+
+    if (embeds && embeds.length > 0) {
+      let embed = embeds[0];
+
+      let icon = embed.author.iconURL;
+      let desc = embed.description;
+
+      // * Wild
+      if (icon === wildIcon) {
+        let user = desc.substr(0, desc.indexOf("found a wild"));
+
+        setTimeout(() => {
+          message.channel.send(`${user}your **pokemon** is ready`);
+        }, 11 * 1000);
+      }
+
+      // * Fish
+      if (icon === waveIcon) {
+        let user = desc.substr(0, desc.indexOf("cast out a"));
+
+        setTimeout(() => {
+          message.channel.send(`${user} your **fish** is ready`);
+        }, 25 * 1000);
+      }
     }
   });
 });
